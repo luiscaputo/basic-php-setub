284d6f316c268528a37dfb152bb81c0294e108bb
luiscaputo <luiscaputo15@gmail.com>
2021-12-30 04:55:35 +0100

    :construction: Folders

---
+++ modules/users/implementations/methods.class.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../../../connection.php';
+class Methods
+{
+  function __construtor()
+  {
+    global $pdo;
+  }
+  public function index()
+  {
+  }
+  public function all()
+  {
+  }
+  public function delete()
+  {
+  }
+  public function aupdate()
+  {
+  }
+  public function geUser()
+  {
+  }
+}
